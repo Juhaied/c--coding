5bf540ff177291cb7f72e2a55e2ea3d9e8a314a9
Juhaied <juhaied.hossen@northsouth.edu>
2023-02-26 15:42:10 +0600

    Some Changes

--- src/assignment_spring2020/Stockable.java
+++
@@ -1,8 +0,0 @@
-package assignment_spring2020;
-
-public interface Stockable {
-	void addStock(int num);
-	void removeStock(int num);
-	void editeStock(int num);
-
-}
--- src/module-info.java
+++
@@ -1,2 +0,0 @@
-module CSE_215_Assignment {
-}
